9bbcc3ff4beee95cb4bb0e2c67d734c278683b29
SilantievMax <nohrin.max@yandex.ru>
2022-04-22 19:08:58 +0300

    add tabs

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', function() {
+    // swiper
     var swiper = new Swiper('.swiper', {
       spaceBetween: 30,
       pagination: {
@@ -6,4 +7,21 @@ window.addEventListener('DOMContentLoaded', function() {
         clickable: true,
       },
     });
-})
+
+    // tabs
+    document.querySelectorAll('.work__item').forEach(function(tabsBtn) {
+      tabsBtn.addEventListener('click', function(e) {
+        const path = e.currentTarget.dataset.path;
+
+        document.querySelectorAll('.work__content-desable').forEach(function(tabContent) {
+          tabContent.classList.remove('work__content-active');
+        })
+        document.querySelector(`[data-target="${path}"]`).classList.add('work__content-active');
+        document.querySelectorAll('.work__item').forEach(function(tabContent) {
+          tabContent.classList.remove('work__btn-active');
+        })
+        document.querySelector(`[data-path="${path}"]`).classList.add('work__btn-active');
+      });
+    });
+
+});
