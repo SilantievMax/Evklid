0a9d5423304566de3da146668d27c4d23675d915
SilantievMax <nohrin.max@yandex.ru>
2022-04-22 13:36:37 +0300

    add swiper

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', function() {
+    var swiper = new Swiper('.swiper', {
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+})
